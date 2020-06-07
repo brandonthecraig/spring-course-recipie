3a3ff3c1b47d9b7194d3321c6c4b5d1eeb643447
craigb <BrandonCraig@rentalcars.com>
2020-06-07 12:44:19 +0100

    First Commit

---
+++ src/test/java/com/brandon/learn/recipie/RecipieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brandon.learn.recipie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
